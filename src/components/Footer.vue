62d61b693eaf282fdd223dbcd617fb617ea35d16
daivd-byte <davidanderson04@live.com>
2021-10-03 15:43:09 -0300

    criando componente Footer e NavBar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,22 @@
 <template>
-  <p>footer</p>
+ <footer id="footer">
+   <p>Make Your Burger &copy; 2021</p>
+ </footer>
 </template>
 <script>
 export default {
-  name: "footer",
+  name: "Footer",
 };
 </script>
+<style scoped>
+#footer {
+  height: 230px;
+  background-color: #222;
+  border-top: 4px solid #111;
+  color: #FCBA03;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
